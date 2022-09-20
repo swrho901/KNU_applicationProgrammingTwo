9ee010658b16ba28a1b1f414aaaf28f6b3291b0f
dnckdgml33 <50548939+dnckdgml33@users.noreply.github.com>
2022-09-20 14:44:20 +0900

    220920
    수정

---
+++ 9월20일_1차/petKing/app/src/main/java/com/example/petking/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.petking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity   {
+
+    ListView pet_listView;
+    PetListViewAdapter pet_adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        Button btnLogout = findViewById(R.id.main_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutUser();
+            }
+        });
+
+        TextView tvName = findViewById(R.id.tvName);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+        tabHost.setup();
+        TabHost.TabSpec spec;
+
+        // 탭에 넣을 이미지 설정
+        ImageView tabwidget01 = new ImageView(this);
+        tabwidget01.setImageResource(R.drawable.ic_baseline_chat_24);
+
+        ImageView tabwidget02 = new ImageView(this);
+        tabwidget02.setImageResource(R.drawable.ic_baseline_comm_24);
+
+        ImageView tabwidget04 = new ImageView(this);
+        tabwidget04.setImageResource(R.drawable.ic_baseline_my_info_24);
+
+        //탭에 이미지 생성한 것 넣어주기
+        TabHost.TabSpec tabFridge = tabHost.newTabSpec("chat").setIndicator(tabwidget01);
+        tabFridge.setContent(R.id.chat);
+        tabHost.addTab(tabFridge);
+
+        TabHost.TabSpec tabRecipe = tabHost.newTabSpec("comm").setIndicator(tabwidget02);
+        tabRecipe.setContent(R.id.comm);
+        tabHost.addTab(tabRecipe);
+
+        TabHost.TabSpec tabBookmark = tabHost.newTabSpec("myinfo").setIndicator(tabwidget04);
+        tabBookmark.setContent(R.id.myinfo);
+        tabHost.addTab(tabBookmark);
+
+        pet_listView = (ListView) findViewById(R.id.sqlResult);
+        pet_adapter = new PetListViewAdapter(MainActivity.this);
+        pet_listView.setAdapter(pet_adapter);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if(user != null){
+                    tvName.setText(user.name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void logoutUser(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+}
