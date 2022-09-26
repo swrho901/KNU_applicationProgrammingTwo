b26274ccfc450e9347eb8471a4923913122091d5
swrho901 <91191844+swrho901@users.noreply.github.com>
2022-09-26 14:36:00 +0900

    ㅇㄴㅇ

---
+++ 9월26일_1차/petKing/app/src/main/java/com/example/petking/MainActivity.java
@@ -0,0 +1,220 @@
+package com.example.petking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity   {
+
+    private boolean fabMain_status = false;
+    private FloatingActionButton fabMain;
+    private FloatingActionButton fabCamera;
+    private FloatingActionButton fabEdit;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<PostList> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView); // 아디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+
+        databaseReference = database.getReference("context"); // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    PostList postlist = snapshot.getValue(PostList.class); // 만들어뒀던 객체에 데이터를 담는다.
+                    arrayList.add(postlist); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                    int k = 0;
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던중 에러 발생 시
+                Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+
+        adapter = new CustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+
+        fabMain = findViewById(R.id.fabMain);
+        fabCamera = findViewById(R.id.fabCamera);
+        fabEdit = findViewById(R.id.fabEdit);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        Button btnLogout = findViewById(R.id.main_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutUser();
+            }
+        });
+
+        TextView tvName = findViewById(R.id.tvName);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+        tabHost.setup();
+        TabHost.TabSpec spec;
+
+        // 탭에 넣을 이미지 설정
+        ImageView tabwidget01 = new ImageView(this);
+        tabwidget01.setImageResource(R.drawable.ic_baseline_main_24);
+
+        ImageView tabwidget02 = new ImageView(this);
+        tabwidget02.setImageResource(R.drawable.ic_baseline_chat_24);
+
+        ImageView tabwidget03 = new ImageView(this);
+        tabwidget03.setImageResource(R.drawable.ic_baseline_comm_24);
+
+        ImageView tabwidget04 = new ImageView(this);
+        tabwidget04.setImageResource(R.drawable.ic_baseline_my_info_24);
+
+        //탭에 이미지 생성한 것 넣어주기
+        TabHost.TabSpec tabMain = tabHost.newTabSpec("main").setIndicator(tabwidget01);
+        tabMain.setContent(R.id.main);
+        tabHost.addTab(tabMain);
+
+        TabHost.TabSpec tabChat = tabHost.newTabSpec("chat").setIndicator(tabwidget02);
+        tabChat.setContent(R.id.chat);
+        tabHost.addTab(tabChat);
+
+        TabHost.TabSpec tabComm = tabHost.newTabSpec("comm").setIndicator(tabwidget03);
+        tabComm.setContent(R.id.comm);
+        tabHost.addTab(tabComm);
+
+        TabHost.TabSpec tabMyinfo = tabHost.newTabSpec("myinfo").setIndicator(tabwidget04);
+        tabMyinfo.setContent(R.id.myinfo);
+        tabHost.addTab(tabMyinfo);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if(user != null){
+                    tvName.setText(user.name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // 메인플로팅 버튼 클릭
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleFab();
+
+            }
+        });
+
+        // 카메라 플로팅 버튼 클릭
+        fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "카메라 버튼 클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 수정 플로팅 버튼 클릭
+        fabEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), writeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void logoutUser(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    // 플로팅 액션 버튼 클릭시 애니메이션 효과
+    public void toggleFab() {
+        if(fabMain_status) {
+            // 플로팅 액션 버튼 닫기
+            // 애니메이션 추가
+            ObjectAnimator fc_animation = ObjectAnimator.ofFloat(fabCamera, "translationY", 0f);
+            fc_animation.start();
+            ObjectAnimator fe_animation = ObjectAnimator.ofFloat(fabEdit, "translationY", 0f);
+            fe_animation.start();
+            // 메인 플로팅 이미지 변경
+            fabMain.setImageResource(R.drawable.ic_baseline_add_24);
+
+        }else {
+            // 플로팅 액션 버튼 열기
+            ObjectAnimator fc_animation = ObjectAnimator.ofFloat(fabCamera, "translationY", -300f);
+            fc_animation.start();
+            ObjectAnimator fe_animation = ObjectAnimator.ofFloat(fabEdit, "translationY", -600f);
+            fe_animation.start();
+            // 메인 플로팅 이미지 변경
+            fabMain.setImageResource(R.drawable.ic_baseline_clear_24);
+        }
+        // 플로팅 버튼 상태 변경
+        fabMain_status = !fabMain_status;
+    }
+
+}
